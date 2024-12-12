fc9ed5155537bc10021501e973ae5eed3e55dc5d
Matthew Mueller <mattmuelle@gmail.com>
2024-12-12 15:53:04 +0700

    support clearing the cache

--- cache/memory.go
+++ cache/memory.go
@@ -7,31 +7,37 @@ import (
 )
 
 // Memory returns a new in-memory cache.
-func Memory() Interface {
-	return &memory{
+func Memory() *Mem {
+	return &Mem{
 		files: map[string]*virt.File{},
 		links: map[string][]string{},
 	}
 }
 
-type memory struct {
+// Mem is an in-memory cache.
+type Mem struct {
 	files map[string]*virt.File
 	links map[string][]string
 }
 
-func (m *memory) Get(path string) (*virt.File, error) {
+func (m *Mem) Get(path string) (*virt.File, error) {
 	if file, ok := m.files[path]; ok {
 		return file, nil
 	}
 	return nil, fs.ErrNotExist
 }
 
-func (m *memory) Set(path string, file *virt.File) error {
+func (m *Mem) Set(path string, file *virt.File) error {
 	m.files[path] = file
 	return nil
 }
 
-func (m *memory) Link(from string, toPatterns ...string) error {
+func (m *Mem) Link(from string, toPatterns ...string) error {
 	m.links[from] = toPatterns
 	return nil
+}
+
+func (m *Mem) Clear() {
+	m.files = map[string]*virt.File{}
+	m.links = map[string][]string{}
 }
